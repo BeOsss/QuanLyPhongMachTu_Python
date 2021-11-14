f43cf148c28e302bc8e883a55bcabfe43c021681
BeOsss <thangtiem01@gmail.com>
2021-11-14 13:55:52 +0700

    add base.html, navbar/index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import flask
-from app import Config
+from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import Flask, render_template
+
 from app import app
-from flask import flask, render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
